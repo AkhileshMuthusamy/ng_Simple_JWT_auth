eee9f0a19bceca8c069ce4fe1c314bc1adb6167f
Akhilesh <AkhileshM@outlook.in>
2019-09-03 19:46:07 +0530

    Added api get call to test jwt token logic

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { detect } from 'detect-browser';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-post',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post.component.css']
 })
 export class PostComponent implements OnInit {
-
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
-  }
+    const browser = detect();
+    // handle the case where we don't detect the browser
+    if (browser) {
+      console.log(browser.name);
+      console.log(browser.version);
+      console.log(browser.os);
+    }
 
+    this.http.get('http://localhost:3000/api/posts').subscribe(data => {
+      console.dir(data);
+    });
+  }
 }
